f07b733c07b8ca42627dfcb718b757fc7682d918
Kan <kan.peljto@stu.ibu.edu.ba>
2024-04-02 10:04:52 +0200

    Reuploaded everything

--- RecipeApp/App.js
+++ RecipeApp/App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { recipes } from './data/recipes';
+import RecipeCard from './components/RecipeCard';
+import { ScrollView } from 'react-native';
+import { Image } from 'react-native';
+import Recipe from './components/Recipe';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+      source={require('./assets/central-icon.png')}
+      style={styles.icon}
+    />
+
+    <ScrollView style={styles.scrollView}>
+      {recipes.map(recipe => (
+        <RecipeCard key={recipe.id} recipe={recipe} />
+      ))}
+    
+      
+    </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#f5fcff',
   },
+
+  iconBarContainer: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'orange',
+  },
+
+  icon: {
+    width:50,
+    height: 50,
+    marginBottom: 40,
+    backgroundColor:'red'
+  },
+
+  scrollView: {
+    width:'80%',
+  }
+  
 });
+
+export default App;
---
+++ RecipeApp/data/recipes.js
@@ -0,0 +1,27 @@
+export const recipes = [
+    {
+      id: 1,
+      title: 'Chicken Soup',
+      image: require('../assets/chicken-soup.jpeg'),
+      calories: 300,
+    },
+    {
+      id: 2,
+      title: 'Beef Stew',
+      image: require('../assets/beef-stew.jpeg'),
+      calories: 500,
+    },
+    {
+      id: 3,
+      title: 'Vegetable Salad',
+      image: require('../assets/vegetable-salad.jpeg'),
+      calories: 100,
+    },
+
+    {
+      id: 4,
+      title: 'Pizza',
+      image: require('../assets/pizza.jpeg'),
+      calories: 800,
+    }
+  ];
