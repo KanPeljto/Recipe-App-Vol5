e669c9e2dd0cddf1aae548419f9fccd336e6c047
Kan <kan.peljto@stu.ibu.edu.ba>
2024-04-05 12:47:44 +0200

    First milestone, only 1 commit because of issues with repositories, more in comment

--- RecipeApp/App.tsx
+++ RecipeApp/App.tsx
@@ -11,6 +11,12 @@ import ProfileTab from './components/ProfileTab';
 import Register from './components/Register';
 import Login from './components/Login';
 
+
+//I had a lot of issues making the repository and then later creating a new branch, 
+//for some reason it would commit to a project from last year? 
+//So i commited regularily in the main branch, 
+//but the future branches will be created normally as i now have the main branch to use as source 
+
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
