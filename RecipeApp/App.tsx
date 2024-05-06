bbb8d519f1d5f7d40f99bc98faf7507cdc072620
KanPeljto <116578415+KanPeljto@users.noreply.github.com>
2024-05-06 11:58:00 +0200

    Made simple tab navigation, working on implementation and working touchable opacity/buttons

--- RecipeApp/App.tsx
+++ RecipeApp/App.tsx
@@ -12,76 +12,31 @@ import Register from './components/Register';
 import Login from './components/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import RecipesTab from './components/RecipesTab';
 
-
-
-
-const Tab= createMaterialTopTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      {/* <Image 
-        source={require('./assets/central-icon.png')}
-        style={styles.icon}
-      /> */}
-
-    
-
-      
       <NavigationContainer>
-        <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Profile' component={ProfileTab}/>
+        <Tab.Navigator style={styles.tabNavigator}>
+          <Tab.Screen name='Home' component={Home}/>
+          <Tab.Screen name='Recipes' component={RecipesTab} />
+          <Tab.Screen name='Profile' component={ProfileTab}/>
         </Tab.Navigator>
       </NavigationContainer>
-      {/* <NavigationBar/> */}
-      <Home/>
-      {/* <ScrollView style={styles.scrollView}>
-       
-      </ScrollView> */}
-      {/* <Favorites/> */}
-      {/* <CategoryIcons/>
-      <ScrollView>
-      {recipes.map(recipe => (
-          <View key={recipe.id}>
-            <RecipeCard recipe={recipe} />
-          </View>
-        ))}
-        </ScrollView> */}
-
-      {/* <RecipeDetails recipe={recipes[0]} /> */}
-
-        {/* <Register/> */}
-
-        {/* <Login/> */}
-
-        
-
-      
-
-       {/* <ProfileTab/> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding: 10,
+    flex: 1,
     backgroundColor: '#f5fcff',
   },
-  icon: {
-    width: 50,
-    height: 50,
-    marginBottom: 40,
-    backgroundColor: 'red',
-  },
-  scrollView: {
-    width: '80%',
+  tabNavigator: {
+    flex: 1,
   },
 });
 
